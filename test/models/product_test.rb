a1d5a5d09ed5e9ac6884a98761f85369615dcaf1
sahilshekhawat <sahilshekhawat01@gmail.com>
2014-05-20 23:26:00 +0530

    Added cart

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -16,12 +16,13 @@ class ProductTest < ActiveSupport::TestCase
  	             ["has already been taken"], product.errors[:title]
   end 	
 
-  test "product attribute must not be empty" do
-  	assert product.invalid?
-  	assert product.errors[:title].any?
-  	assert product.errors[:description].any?
-  	assert product.errors[:price].any?
-  	assert product.errors[:image_url].any?
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
   end
 
   test "product price must be positive" do
@@ -35,27 +36,28 @@ class ProductTest < ActiveSupport::TestCase
 
   	product.price = 0
   	assert product.invalid?
-  	assert_equal ["must be greater than or equal to 0.01"]
+  	assert_equal ["must be greater than or equal to 0.01"],
   		product.errors[:price]
 
   	product.price = 1
-  	assert product.invalid?
+  	assert product.valid?
   end
 
   def new_product(image_url)
   	Product.new(title: "two states",
   				description: "favourite book",
-  				proce: 1,
+  				price: 1,
   				image_url: image_url)
   end
+
   test "image_url" do
   	ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
-  			http://a.b.c/x/y/z/fred.git }
+  			http://a.b.c/x/y/z/fred.gif }
   	bad = %w{ fred.doc fred.gitignore fred.git.more }
-  	ok.each.do |name|
-  		assert new_production(name).valid?, "#{name} should be valid"
+  	ok.each do |name|
+  		assert new_product(name).valid?, "#{name} should be valid"
   	end
-  	bad.each.do |name|
+  	bad.each do |name|
   		assert new_product(name).invalid?, "#{name} shoudn't be valid"
   	end
   end		
